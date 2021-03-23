2aadc1f4c51f19248bafc0fa6abe48d41647b5a9
jack beagle <jackbeagle@live.co.uk>
2021-03-23 01:07:58 +0000

    Changed some mobile css styling for better image sizing

--- reddit/src/app/Header/Header.js
+++ reddit/src/app/Header/Header.js
@@ -5,12 +5,6 @@ function Header() {
     return (
         <div className="Header">
             <h1>Reddit App</h1>
-            <div className="SearchBar">
-                <input
-                    placeholder="Search These Posts"
-                />
-                <button className="SearchButton" >Search</button>
-            </div>
         </div>
     )
 }
